51d613ee80bc5e37af447f78fb5c24e6bd153a2a
nverchev <nicolas.vercheval@gmail.com>
2023-11-27 19:38:53 +0100

    import from local path

--- src/custom_trainer/__init__.py
+++ src/custom_trainer/__init__.py
@@ -1,8 +1,8 @@
 __all__ = ['Scheduler', 'ConstantScheduler', 'ExponentialScheduler', 'CosineScheduler', 'get_scheduler',
            'DictList', 'Trainer']
 
-from src.custom_trainer.schedulers import Scheduler, ConstantScheduler, ExponentialScheduler, CosineScheduler, \
+from .schedulers import Scheduler, ConstantScheduler, ExponentialScheduler, CosineScheduler, \
     get_scheduler
-from src.custom_trainer.trainer import Trainer
-from src.custom_trainer.utils import DictList, apply, dict_repr, C
+from .trainer import Trainer
+from .utils import DictList, apply, dict_repr, C
 
