10edabf33f808987dece1942146ecb84e7afbfd8
nverchev <nicolas.vercheval@gmail.com>
2024-04-25 17:02:23 +0200

    Added Pyaml and importlib for testing.

--- dry_torch/model_optimizer.py
+++ dry_torch/model_optimizer.py
@@ -1,10 +1,14 @@
-from typing import Generic, Optional, Any, Self
+from typing import TypeVar, Generic, Optional, Any, Self
 import copy
 
 import torch
 
-from dry_torch import Scheduler, ConstantScheduler
-from dry_torch.protocols import TypedModule, OptParams, ModuleInput, ModuleOutput
+from .schedulers import Scheduler, ConstantScheduler
+from .experiment_info import extract_metadata, ExpInfo
+from .protocols import TypedModule, OptParams
+
+ModuleInput = TypeVar('ModuleInput', contravariant=True)
+ModuleOutput = TypeVar('ModuleOutput', covariant=True)
 
 
 class ModelOptimizer(Generic[ModuleInput, ModuleOutput]):
@@ -21,6 +25,7 @@ class ModelOptimizer(Generic[ModuleInput, ModuleOutput]):
         device: the device where to store the model. Default uses cuda when available else cpu.
         epoch: the current epoch, that is, the number of epochs the model has been trainer plus one. Defaults to 0.
 
+
     Attributes:
         model: Pytorch model to optimize. TypedModule type variables match the class type variables.
         optimizer: the optimizer bound to the model.
@@ -33,7 +38,11 @@ class ModelOptimizer(Generic[ModuleInput, ModuleOutput]):
         update_learning_rate(): update the optimizer with the updated settings or with an input learning rate.
     """
 
+    @extract_metadata
     def __init__(self, model: TypedModule[ModuleInput, ModuleOutput] | torch.nn.Module,
+                 /,
+                 *,
+                 exp_info: ExpInfo,
                  optimizer_cls: type[torch.optim.Optimizer] = torch.optim.Adam,
                  lr: float | dict[str, float] = 0.001,
                  other_optimizer_args: Optional[dict[str, Any]] = None,
@@ -42,13 +51,15 @@ class ModelOptimizer(Generic[ModuleInput, ModuleOutput]):
                  epoch: int = 0,
                  ) -> None:
         default_device: torch.device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
+        self.exp_info: ExpInfo = exp_info
         self.device: torch.device = default_device if device is None else device
-        self.model: TypedModule[ModuleInput, ModuleOutput] | torch.nn.Module = model.to(device)
-        self.params_lr = lr
+        self.model: TypedModule[ModuleInput, ModuleOutput] | torch.nn.Module = model.to(self.device)
+        self.params_lr_setter(lr)
         self.scheduler: Scheduler = scheduler
         self.epoch: int = epoch
         optimizer_args: dict[str, Any] = {} if other_optimizer_args is None else other_optimizer_args
         self.optimizer: torch.optim.Optimizer = optimizer_cls(params=self.params_lr, **optimizer_args)
+        self._params_lr: list[OptParams] = []
 
     def clone(self) -> Self:
         """
@@ -68,8 +79,8 @@ class ModelOptimizer(Generic[ModuleInput, ModuleOutput]):
         """
         return [OptParams(params=g['params'], lr=self.scheduler(g['lr'], self.epoch)) for g in self._params_lr]
 
-    @params_lr.setter
-    def params_lr(self, lr: float | dict[str, float]) -> None:
+    # params_lr.setter does not implement typing suggestion
+    def params_lr_setter(self, lr: float | dict[str, float]) -> None:
         """
         Pair the learning rates with the parameter groups.
 
@@ -93,11 +104,11 @@ class ModelOptimizer(Generic[ModuleInput, ModuleOutput]):
                If None (default), the scheduled original learning rates are used. Else, the scheduler is deactivated.
         """
         if lr is not None:
-            self.params_lr = lr
+            self.params_lr_setter(lr)
             self.scheduler = ConstantScheduler()
         for g, up_g in zip(self.optimizer.param_groups, self.params_lr):
             g['lr'] = up_g['lr']
         return
 
     def __repr__(self):
-        return f'ModelOptimizer(model={self.model}, optimizer={self.optimizer}, epoch={self.epoch})'
+        return self.__class__.__name__ + f'(model={self.model}, optimizer_cls={self.optimizer.__class__.__name__})'
