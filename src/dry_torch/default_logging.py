80e2a18f0d5886a7fa905e61c08df8f89e499a66
nverchev <nicolas.vercheval@gmail.com>
2024-05-30 18:37:24 +0200

    One handler with no filters. One additional Info level. Better formatting for epoch level.

--- src/dry_torch/default_logging.py
+++ src/dry_torch/default_logging.py
@@ -1,53 +1,41 @@
 import sys
 import logging
-from typing import NamedTuple, Optional, Final
+from typing import NamedTuple, Optional
 
 
 class InfoLevels(NamedTuple):
-    checkpoint: int
-    experiment: int
+    progress_bar: int
     metrics: int
     epoch: int
+    checkpoint: int
     training: int
-
-
-class InfoFilter(logging.Filter):
-    def __init__(self, level_no: Optional[int] = None, name='') -> None:
-        super().__init__(name)
-        self.level_no: Final = level_no
-
-    def filter(self, record) -> bool:
-        if self.level_no is None:
-            return True
-        return record.levelno == self.level_no
+    experiment: int
 
 
 class InfoFormatter(logging.Formatter):
 
-    def __init__(self,
-                 fmt: Optional[str] = None,
-                 datefmt='%H:%M:%S %d-%m-%Y',
-                 *args,
-                 level_no: Optional[int] = None,
-                 defaults=None) -> None:
-        self.default_fmt: Final = fmt
-        fmt = self._info_fmt(level_no)
-        super().__init__(fmt, datefmt, *args, defaults=defaults)
+    def format(self, record: logging.LogRecord) -> str:
+        self._style._fmt = self._info_fmt(record.levelno)
+        return super().format(record)
 
-    def _info_fmt(self, level_no: Optional[int] = None) -> Optional[str]:
+    @staticmethod
+    def _info_fmt(level_no: Optional[int] = None) -> str:
         if level_no == INFO_LEVELS.training:
-            return '%(asctime)s\n%(message)s'
-        return self.default_fmt
+            return '%(asctime)s\n%(message)s\n'
+        if level_no == INFO_LEVELS.epoch:
+            if logger.level > INFO_LEVELS.metrics:
+                return '%(message)s ...\r'
+        return '%(message)s\n'
 
 
 def set_default_logging() -> None:
     logger.handlers.clear()
-    for level_name, level_no in INFO_LEVELS._asdict().items():
-        formatter = InfoFormatter(level_no=level_no)
-        stdout_handler = logging.StreamHandler(sys.stdout)
-        stdout_handler.setFormatter(formatter)
-        stdout_handler.addFilter(InfoFilter(level_no, level_name))
-        logger.addHandler(stdout_handler)
+    formatter = InfoFormatter()
+
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.terminator = ''
+    stdout_handler.setFormatter(formatter)
+    logger.addHandler(stdout_handler)
     logger.setLevel(INFO_LEVELS.checkpoint)
     logger.propagate = False
 
@@ -60,5 +48,5 @@ def propagate_to_main_logger() -> None:
 
 
 logger = logging.getLogger('dry_torch')
-INFO_LEVELS = InfoLevels(17, 21, 23, 25, 27)
+INFO_LEVELS = InfoLevels(17, 21, 23, 25, 27, 28)
 set_default_logging()
