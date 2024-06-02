3f17fa0d15a246a013affe8530fc29555c5a7363
nverchev <nicolas.vercheval@gmail.com>
2024-06-02 16:23:17 +0200

    Separation Log / Test / Trainer.

--- src/dry_torch/training.py
+++ src/dry_torch/training.py
@@ -3,12 +3,11 @@ from __future__ import annotations
 import sys
 import logging
 
-from typing import Callable, Optional, Self, TypeVar, Generic
+from typing import Callable, Optional, Self, TypeVar, Generic, Generator
 import pandas as pd
 import torch
 from torch.cuda import amp
 
-from dry_torch import loss_and_metrics
 from dry_torch import exceptions
 from dry_torch import tracking
 from dry_torch import model_binding
@@ -26,7 +25,53 @@ _Output = TypeVar('_Output', bound=data_types.OutputType)
 logger = logging.getLogger('dry_torch')
 
 
+class LogMetrics(object):
+    def __init__(self, model_name: str, partition: data_types.Split) -> None:
+        self.model_name = model_name
+        self.partition = partition
+
+    @property
+    def model_info(self) -> tracking.ModelTracking:
+        exp = tracking.Experiment.get_active_environment()
+        return exp.model[self.model_name]
+
+    def __call__(self,
+                 metrics: dict[str, float]) -> None:
+        log_msg_list: list[str] = ['Average %(split)s metric(s):']
+        log_args: dict[str, str | float] = {
+            'split': self.partition.name.lower()
+        }
+
+        partition_log: pd.DataFrame = self.model_info.log[self.partition]
+        for metric, value in metrics.items():
+            partition_log.loc[self.model_info.epoch, metric] = value
+            log_msg_list.append(f'%({metric})s: %({metric}_value)4e')
+            log_args.update({metric: metric, f'{metric}_value': value})
+        logger.log(default_logging.INFO_LEVELS.metrics,
+                   '\t'.join(log_msg_list),
+                   log_args)
+
+    def log_train(self, num_epochs: int) -> Generator[None, None, None]:
+        logger.log(default_logging.INFO_LEVELS.training,
+                   'Training %(model_name)s.',
+                   {'model_name': self.model_name})
+        # if self.early_termination:
+        #     logger.warning('Attempted to train model after termination.')
+        for _ in range(num_epochs):
+            self.model_info.epoch += 1
+            epoch_msg = '====> Epoch %(epoch)4d:'
+            logger.log(default_logging.INFO_LEVELS.epoch,
+                       epoch_msg, {'epoch': self.model_info.epoch})
+            try:
+                yield
+            except exceptions.ConvergenceError as ce:
+                logger.error(ce)
+
+        logger.log(default_logging.INFO_LEVELS.training, 'End of training.')
+
+
 class Test(Generic[_Input, _Target, _Output]):
+    partition = data_types.Split.TEST
     max_stored_output: int = sys.maxsize
 
     """
@@ -49,7 +94,9 @@ class Test(Generic[_Input, _Target, _Output]):
         update_frequency:
         number of times the progress bar updates in one epoch.
         test_outputs:
-        An instance of TorchDictList that stores the last test evaluation.
+            An instance of TorchDictList that stores the last test evaluation.
+        save_outputs: if the flag is active store the model outputs in the
+            test_outputs attribute. Default to False.
 
     Methods:
         train:
@@ -68,85 +115,56 @@ class Test(Generic[_Input, _Target, _Output]):
             /,
             *,
             loader: protocols.LoaderProtocol[_Input, _Target],
-            metrics_calc: protocols.MetricsCallable[_Output, _Target]
+            metrics_calc: protocols.MetricsCallable[_Output, _Target],
+            save_outputs: bool = False,
     ) -> None:
 
         self._model_optimizer = model_optimizer
         self._loader = loading.TqdmLoader[_Input, _Target](loader)
         self._metrics_calc = metrics_calc
+        self.save_outputs = save_outputs
         self.test_outputs = structures.TorchDictList()
-        self._reduce_metrics = metrics_calc.output_class.reduce_metrics
-
+        self.log_metrics = LogMetrics(model_optimizer.name,
+                                      partition=self.partition)
         return
 
-    @property
-    def model_info(self) -> tracking.ModelTracking:
-        exp = tracking.Experiment.get_active_environment()
-        return exp.model[self._model_optimizer.name]
-
     @torch.inference_mode()
-    def __call__(self,
-                 partition: data_types.Split = data_types.Split.VAL,
-                 save_outputs: bool = False) -> None:
+    def __call__(self) -> None:
         """
         Evaluates the model's performance on the specified partition of the
         dataset.
 
         Parameters:
-            partition: The partition of the dataset on which to evaluate the
-            model's performance.  Default to 'val'.
-            save_outputs: if the flag is active store the model outputs in the
-            test_outputs attribute. Default to False.
+
 
         """
-        if save_outputs:
+        if self.save_outputs:
             self.test_outputs.clear()
         self._model_optimizer.model.eval()
-        self._run_epoch(partition=partition)
+        self._run_epoch()
         return
 
-    def _run_epoch(self,
-                   partition: data_types.Split) -> None:
+    def _run_epoch(self) -> None:
         """
            Run a single epoch of training or evaluation.
-
-           Parameters:
-               partition: The partition of the dataset on which to evaluate
-                the model's performance.
-
            """
-        metrics = loss_and_metrics.MetricsAggregate(float)
-        for inputs, targets in self._loader:
-            metrics += self._run_batch(inputs, targets)
-        self._log_metrics(partition, self._reduce_metrics(metrics))
+        metrics = structures.TorchAggregate()
+        for batch in self._loader:
+            metrics += self._batched_metrics(batch)
+        self.log_metrics(metrics.reduce())
         return
 
-    def _log_metrics(self,
-                     partition: data_types.Split,
-                     metrics: dict[str, float]) -> None:
-        log_msg_list: list[str] = ['Average %(split)s metric(s):']
-        log_args: dict[str, str | float] = {'split': partition.name.lower()}
-
-        partition_log: pd.DataFrame = self.model_info.log[partition]
-        for metric, value in metrics.items():
-            partition_log.loc[self.model_info.epoch, metric] = value
-            log_msg_list.append(f'%({metric})s: %({metric}_value)4e')
-            log_args.update({metric: metric, f'{metric}_value': value})
-        logger.log(default_logging.INFO_LEVELS.metrics,
-                   '\t'.join(log_msg_list),
-                   log_args)
-
-    def _run_batch(
+    def _batched_metrics(
             self,
-            inputs: _Input,
-            targets: _Target,
-    ) -> protocols.AggregateMapping:
-        inputs, targets = (
-            recursive_ops.recursive_to(
-                [inputs, targets], self._model_optimizer.device
-            )
-        )
+            batch: tuple[_Input, _Target]
+    ) -> dict[str, torch.Tensor]:
+        device = self._model_optimizer.device
+        inputs, targets = recursive_ops.recursive_to(batch, device)
         outputs = self._model_optimizer(inputs)
+        if self.save_outputs:
+            self.test_outputs.extend(
+                structures.TorchDictList.from_batch(outputs)
+            )
         batched_performance = self._metrics_calc(outputs, targets)
         return batched_performance.metrics
 
@@ -154,7 +172,8 @@ class Test(Generic[_Input, _Target, _Output]):
         return f'Trainer for {self._model_optimizer.name}.'
 
 
-class Trainer(protocols.TrainerProtocol, Generic[_Input, _Target, _Output]):
+class Trainer(Test[_Input, _Target, _Output], protocols.TrainerProtocol):
+    partition = data_types.Split.TRAIN
     """
     Implement the standard Pytorch training and evaluation loop.
 
@@ -192,44 +211,45 @@ class Trainer(protocols.TrainerProtocol, Generic[_Input, _Target, _Output]):
             mixed_precision: bool = False,
     ) -> None:
 
-        self._model_optimizer = model_optimizer
-        self._loader = loading.TqdmLoader[_Input, _Target](train_loader)
+        super().__init__(model_optimizer,
+                         loader=train_loader,
+                         metrics_calc=loss_calc)
         device_is_cuda = model_optimizer.device.type == 'cuda'
         enable_mixed_precision = mixed_precision and device_is_cuda
         self._scaler = amp.GradScaler(enabled=enable_mixed_precision)
         self._mixed_precision = mixed_precision
 
         self._loss_calc = loss_calc
-        self._reduce_metrics = loss_calc.output_class.reduce_metrics
         self._pre_epoch_hooks: list[Callable[[Self], None]] = []
         self._post_epoch_hooks: list[Callable[[Self], None]] = []
 
         if val_loader is None:
-            self._val_loader = None
+            self.validation_test: Callable[[], None] = lambda: None
         else:
-            self._val_loader = loading.TqdmLoader[_Input, _Target](val_loader)
+            self.validation_test = Test(model_optimizer,
+                                        loader=val_loader,
+                                        metrics_calc=self._loss_calc)
+            self.validation_test.partition = data_types.Split.VAL
             self._activate_validation()
 
         self.early_termination = False
         return
 
     def _activate_validation(self: Self) -> None:
-        def validate_self(instance: Self) -> None:
+        def validate(instance: Self) -> None:
             instance.validate()
             return
 
-        self._post_epoch_hooks.append(validate_self)
+        self._post_epoch_hooks.append(validate)
         return
 
-    @property
-    def model_info(self) -> tracking.ModelTracking:
-        exp = tracking.Experiment.get_active_environment()
-        return exp.model[self._model_optimizer.name]
+    def validate(self) -> None:
+        self.validation_test()
+        return
 
     def terminate_training(self) -> None:
         self.early_termination = True
-        logger.log(default_logging.INFO_LEVELS.training,
-                   'Training has been terminated.')
+        return
 
     def train(self, num_epoch: int, val_after_train: bool = False) -> None:
         """
@@ -240,94 +260,42 @@ class Trainer(protocols.TrainerProtocol, Generic[_Input, _Target, _Output]):
             val_after_train: if the flag is active, evaluate loss function
             on the validation dataset. Default to False.
         """
-        logger.log(default_logging.INFO_LEVELS.training,
-                   'Training %(model_name)s.',
-                   {'model_name': self._model_optimizer.name})
-        if self.early_termination:
-            logger.warning('Attempted to train model after termination.')
-        for _ in range(num_epoch):
+        train_logger = self.log_metrics.log_train(num_epoch)
+        for _ in train_logger:
             if self.early_termination:
                 return
             self._model_optimizer.update_learning_rate()
-            self.model_info.epoch += 1
-
-            # Logging
-            epoch_msg = '====> Epoch %(epoch)4d:'
-            logger.log(default_logging.INFO_LEVELS.epoch,
-                       epoch_msg, {'epoch': self.model_info.epoch})
-
             self._model_optimizer.model.train()
             self.exec_pre_epoch_hooks()
             try:
-                self._run_epoch(partition=data_types.Split.TRAIN)
+                self._run_epoch()
             except exceptions.ConvergenceError as ce:
-
-                # Logging
-                logger.error(ce)
-                self.terminate_training()
-
+                train_logger.throw(ce)
+                self.early_termination = True
             self.exec_post_epoch_hooks()
-        logger.log(default_logging.INFO_LEVELS.training, 'End of training.')
 
-    def validate(self):
-        self._model_optimizer.model.eval()
-        with torch.inference_mode():
-            self._run_epoch(partition=data_types.Split.VAL)
+    def _batched_metrics(
+            self,
+            batch: tuple[_Input, _Target]
+    ) -> dict[str, torch.Tensor]:
 
-    def _run_epoch(self,
-                   partition: data_types.Split) -> None:
-        """
-           Run a single epoch of training or evaluation.
-
-           Parameters:
-               partition: The partition of the dataset on which to evaluate
-                the model's performance.
-
-
-           """
-        metrics = loss_and_metrics.MetricsAggregate(float)
-        for inputs, targets in self._loader:
-            metrics += self._run_batch(inputs, targets)
-        self._log_metrics(partition, self._reduce_metrics(metrics))
-        return
-
-    def _log_metrics(self,
-                     partition: data_types.Split,
-                     metrics: dict[str, float]) -> None:
-        log_msg_list: list[str] = ['Average %(split)s metric(s):']
-        log_args: dict[str, str | float] = {'split': partition.name.lower()}
-
-        partition_log: pd.DataFrame = self.model_info.log[partition]
-        for metric, value in metrics.items():
-            partition_log.loc[self.model_info.epoch, metric] = value
-            log_msg_list.append(f'%({metric})s: %({metric}_value)4e')
-            log_args.update({metric: metric, f'{metric}_value': value})
-        logger.log(default_logging.INFO_LEVELS.metrics,
-                   '\t'.join(log_msg_list),
-                   log_args)
-
-    def _run_batch(self,
-                   inputs: _Input,
-                   targets: _Target) -> protocols.AggregateMapping:
         device = self._model_optimizer.device
-        inputs, targets = recursive_ops.recursive_to([inputs, targets], device)
-
+        inputs, targets = recursive_ops.recursive_to(batch, device)
         with torch.autocast(device_type=self._model_optimizer.device.type,
                             enabled=self._mixed_precision):
             outputs = self._model_optimizer(inputs)
             batched_performance = self._loss_calc(outputs, targets)
             criterion: torch.Tensor = batched_performance.criterion
         self._loader.send({'Loss': criterion.item()})
-        if not torch.is_inference_mode_enabled():
-            try:
-                self._scaler.scale(criterion).backward()
-            except ValueError as ve:
-                if torch.isinf(criterion) or torch.isnan(criterion):
-                    raise exceptions.ConvergenceError(criterion.item())
-                raise ve
-            self._scaler.step(self._model_optimizer.optimizer)
-            self._scaler.update()
-            self._model_optimizer.optimizer.zero_grad()
+        try:
+            self._scaler.scale(criterion).backward()
+        except ValueError as ve:
+            if torch.isinf(criterion) or torch.isnan(criterion):
+                raise exceptions.ConvergenceError(criterion.item())
+            raise ve
+        self._scaler.step(self._model_optimizer.optimizer)
+        self._scaler.update()
+        self._model_optimizer.optimizer.zero_grad()
         return batched_performance.metrics
 
     def add_pre_epoch_hook(
