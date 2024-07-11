dbec802b4e0f2840e4946bb0daf5892aa7d55798
nverchev <nicolas.vercheval@gmail.com>
2024-07-11 17:40:18 +0200

    Support for NamedTuples

--- tests/test_dict_list.py
+++ tests/test_dict_list.py
@@ -1,10 +1,21 @@
 import pytest
 import torch
+from typing import NamedTuple, Sequence
+
 from dry_torch import TorchDictList
 from dry_torch.structures import DictList
 from dry_torch.exceptions import KeysAlreadySetError
 from dry_torch.exceptions import DifferentBatchSizeError
-from dry_torch.data_types import Tensors
+
+
+class BatchTuple(NamedTuple):
+    output1: torch.Tensor
+    output2: torch.Tensor
+
+
+class ListedBatchTuple(NamedTuple):
+    output1: list[torch.Tensor]
+    output2: list[torch.Tensor]
 
 
 def test_DictList() -> None:
@@ -43,20 +54,45 @@ def test_DictList() -> None:
     assert list(dict_list.keys()) == ['list1']
 
 
+tuple_list_type = Sequence[
+    tuple[torch.Tensor | tuple[torch.Tensor, ...], ...]
+]
+
+
+def check_equal(tuple_list: tuple_list_type,
+                expected_result: tuple_list_type) -> None:
+    for t1, t2 in zip(tuple_list, expected_result):
+        for t11, t22 in zip(t1, t2):
+            if isinstance(t11, torch.Tensor):
+                assert isinstance(t22, torch.Tensor)
+                assert torch.allclose(t11, t22)
+            else:
+                for t111, t222 in zip(t11, t22):
+                    assert isinstance(t111, torch.Tensor)
+                    assert isinstance(t222, torch.Tensor)
+                    assert torch.allclose(t111, t222)
+    return
+
+
 def test_TorchDictList() -> None:
-    input_dict_torch: dict[str, Tensors] = {
-        'tensor1': torch.ones(2),
-        'list_tensor': [torch.zeros(2)]
-    }
+    batch_tuple = BatchTuple(torch.ones(2, 2), torch.zeros(2, 2))
+    expected_result: tuple_list_type = 2 * [
+        (torch.tensor(1.), torch.tensor(0.))
+    ]
+    tuple_list = TorchDictList.from_batch(batch_tuple)._tuple_list
 
-    # test enlist
-    expected_result = 2 * [(torch.tensor(1), (torch.tensor(0), ))]
-    assert TorchDictList._enlist(input_dict_torch.values()) == expected_result
+    check_equal(tuple_list, expected_result)
 
+    listed_batch_tuple = ListedBatchTuple(
+        [torch.ones(2, 2)], [torch.zeros(2, 2)]
+    )
+    expected_result = 2 * [((torch.ones(2),), (torch.zeros(2),))]
+    tuple_list = TorchDictList.from_batch(listed_batch_tuple)._tuple_list
+
+    check_equal(tuple_list, expected_result)
     # test DifferentBatchSizeError
-    wrong_input_dict_torch: dict[str, Tensors] = {
-        'tensor1': torch.ones(2, 2),
-        'list_tensor': [torch.zeros(1, 2)]
-    }
+    wrong_batch_tuple = ListedBatchTuple(
+        [torch.ones(2, 2)], [torch.zeros(1, 2)]
+    )
     with pytest.raises(DifferentBatchSizeError):
-        TorchDictList.from_batch(wrong_input_dict_torch)
+        TorchDictList.from_batch(wrong_batch_tuple)
--- tests/test_trainer.py
+++ tests/test_trainer.py
@@ -4,7 +4,7 @@ import pytest
 import torch
 from torch.utils import data
 from dry_torch import Trainer
-from dry_torch import Test as ClassTest  # pytest interpret the class as a test
+from dry_torch import Test as _Test  # other pytest interprets it as a test
 from dry_torch import DataLoader
 from dry_torch import Experiment
 from dry_torch import Model
@@ -13,16 +13,24 @@ from dry_torch import exceptions
 from dry_torch import default_logging
 from dry_torch import LearningScheme
 
+from typing import NamedTuple
 
-class IdentityDataset(data.Dataset[tuple[torch.Tensor, torch.Tensor]]):
+
+class TorchTuple(NamedTuple):
+    input: torch.Tensor
+
+
+class IdentityDataset(data.Dataset[tuple[TorchTuple, torch.Tensor]]):
 
     def __init__(self):
         self.tensors = torch.rand(7, 7, 7)
         super().__init__()
 
-    def __getitem__(self, index: int) -> tuple[torch.Tensor, torch.Tensor]:
+    def __getitem__(self, index: int) -> tuple[
+        TorchTuple, torch.Tensor
+    ]:
         x = torch.FloatTensor([index]) / len(self)
-        return x, x
+        return TorchTuple(x), x
 
     def __len__(self) -> int:
         return 1600
@@ -34,16 +42,17 @@ class Linear(torch.nn.Module):
         super().__init__()
         self.linear = torch.nn.Linear(in_features, out_features)
 
-    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
-        return self.linear(inputs)
+    def forward(self, inputs: TorchTuple) -> torch.Tensor:
+        return self.linear(inputs.input)
 
 
-def square_error(outputs: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
+def square_error(outputs: torch.Tensor,
+                 targets: torch.Tensor) -> torch.Tensor:
     return (outputs - targets) ** 2
 
 
 logger = logging.getLogger('dry_torch')
-logger.setLevel(default_logging.INFO_LEVELS.progress_bar)
+logger.setLevel(default_logging.INFO_LEVELS.tqdm_bar)
 
 
 def test_all() -> None:
@@ -75,12 +84,19 @@ def test_all() -> None:
                 calculator=loss_calc,
                 loader=loader,
                 val_loader=loader)
-    out = cloned_model(torch.FloatTensor([.2]).to(cloned_model.device))
+    sample_in = TorchTuple(
+        input=torch.FloatTensor([.2]).to(cloned_model.device)
+    )
+    out = cloned_model(sample_in)
     assert torch.isclose(out, torch.tensor(.2), atol=0.01)
 
-    test = ClassTest(model, calculator=loss_calc, loader=loader)
+    test = _Test(model,
+                 calculator=loss_calc,
+                 loader=loader,
+                 store_outputs=True)
     test()
-    test()
+    with pytest.warns(exceptions.AlreadyTestedWarning):
+        test()
 
 
 if __name__ == '__main__':
