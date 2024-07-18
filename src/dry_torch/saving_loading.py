715a0f3b944abde8f85e6ff7ff1624c1135abf3f
nverchev <nicolas.vercheval@gmail.com>
2024-07-18 12:03:23 +0200

    No filter epochs when loading test.

--- src/dry_torch/saving_loading.py
+++ src/dry_torch/saving_loading.py
@@ -192,8 +192,9 @@ class TrackingIO:
                 df = pd.read_csv(path, index_col=0)
             except FileNotFoundError:
                 df = pd.DataFrame()
-            # filter out future epochs from logs
-            df = df[df.index <= self.model_tracking.epoch]
+            if split is not p.Split.TEST:
+                # filter out future epochs from logs
+                df = df[df.index <= self.model_tracking.epoch]
             self.model_tracking.log[split] = df
         logger.log(default_logging.INFO_LEVELS.checkpoint,
                    f"Loaded %(definition)s at epoch %(epoch)d.",
