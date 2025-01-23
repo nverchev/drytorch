327ac85549239ccf87ae6aa505d5e0cbefe0fdfb
nverchev <nicolas.vercheval@gmail.com>
2025-01-23 13:28:30 +0100

    Improved tests and test PathManager as well.

--- tests/units/test_checkpoint.py
+++ tests/units/test_checkpoint.py
@@ -1,58 +1,104 @@
 """Tests for the checkpoint module."""
 
-import pathlib
-
 import pytest
 
-from src.dry_torch import ModelStateIO
-from src.dry_torch import CheckpointIO
+import torch
+
+from src.dry_torch import checkpoint
 from src.dry_torch import exceptions
+
+
+class TestPathManager:
+
+    @pytest.fixture(autouse=True)
+    def setup(self, mock_model) -> None:
+        """Set up the path manager."""
+        self.manager = checkpoint.PathManager(mock_model)
+        return
+
+    def test_dirs_creation(self, mock_model, experiment):
+        """Test that the directories are created when called."""
+        checkpoint_dir = experiment.dir / mock_model.name / 'checkpoints'
+        epoch_dir = checkpoint_dir / f'epoch_{mock_model.epoch}'
+        expected_dirs = [checkpoint_dir, epoch_dir]
+
+        for expected_dir in expected_dirs:
+            assert not expected_dir.exists()
+
+        dirs = [self.manager.checkpoint_dir, self.manager.epoch_dir]
+
+        for dir_, expected_dir in zip(dirs, expected_dirs):
+            assert dir_ == expected_dir
+            assert dir_.exists()
+            assert dir_.is_dir()
+
+        return
+
+    def test_paths(self):
+        """Test that the paths have the correct name."""
+        epoch_dir = self.manager.epoch_dir
+        paths = [self.manager.state_path, self.manager.optimizer_path]
+        expected_paths = [epoch_dir / 'state.pt', epoch_dir / 'optimizer.pt']
+
+        for path, expected_path in zip(paths, expected_paths):
+            assert path == expected_path
 
 
 class TestModelStateIO:
     @pytest.fixture(autouse=True)
     def setup(self, mock_model):
         """Set up the model state class."""
-        self.model_io = ModelStateIO(mock_model)
-
-    def test_get_last_saved_epoch(self, mocker):
-        """Test it recovers the epoch of the longest trained model."""
-        mock_path_1 = mocker.MagicMock()
-        mock_path_1.stem = 'state_1'
-        mock_path_1.is_dir.return_value = True
-        mock_path_2 = mocker.MagicMock()
-        mock_path_2.stem = 'state_2'
-        mock_path_1.is_dir.return_value = True
-        paths = mocker.MagicMock()
-        paths.checkpoint_dir.iterdir.return_value = [mock_path_1, mock_path_2]
-        self.model_io.paths = paths
-        assert self.model_io._get_last_saved_epoch() == 2
+        self.model_io = checkpoint.ModelStateIO(mock_model)
 
     def test_get_last_saved_epoch_no_checkpoints(self, mocker):
         """Test it raises error if it cannot find any folder."""
-        paths = mocker.MagicMock()
-        paths.checkpoint_dir.iterdir.return_value = []
-        self.model_io.paths = paths
         with pytest.raises(exceptions.ModelNotFoundError):
-            self.model_io._get_last_saved_epoch()
+            self.model_io.load()
+
+    def test_save_and_load(self):
+        """Test it saves the model's state."""
+        self.model_io.save()
+        old_weight = self.model_io.model.module.weight.clone()
+        new_weight = torch.FloatTensor([[0.]])
+        self.model_io.model.module.weight = torch.nn.Parameter(new_weight)
+        assert old_weight != self.model_io.model.module.weight
+        self.model_io.load(self.model_io.model.epoch)
+        assert old_weight == self.model_io.model.module.weight
+
+    def test_get_last_saved_epoch(self, mocker):
+        """Test it recovers the epoch of the longest trained model."""
+        self.model_io.save()
+        old_epoch = self.model_io.model.epoch
+        assert self.model_io._get_last_saved_epoch() == old_epoch
+        new_epoch = 15
+        self.model_io.model.epoch = new_epoch
+        self.model_io.save()
+        assert self.model_io._get_last_saved_epoch() == new_epoch
 
 
 class TestCheckpointIO:
     @pytest.fixture(autouse=True)
     def setup(self, mock_model, mocker):
         """Set up the Checkpoint class."""
-        optimizer = mocker.MagicMock()
-        self.checkpoint_io = CheckpointIO(mock_model, optimizer)
+        optimizer = torch.optim.SGD(mock_model.module.parameters())
+        self.checkpoint_io = checkpoint.CheckpointIO(mock_model, optimizer)
 
-    def test_checkpoint_load_optimizer_warning(self, mocker):
-        """Test it raises a warning if it cannot find the optimizer."""
-        mock_optimizer = mocker.MagicMock()
-        mock_optimizer.load_state_dict.side_effect = ValueError('Test error')
-        mocker.patch('torch.load')
-        ModelStateIO.load = mocker.MagicMock()
-        self.checkpoint_io.optimizer = mock_optimizer
-        paths = mocker.MagicMock()
-        paths.epoch_dir = pathlib.Path()
-        self.checkpoint_io.paths = paths
+    def test_save_and_load(self):
+        """Test it saves the model's state."""
+        self.checkpoint_io.save()
+        old_lr = self.checkpoint_io.optimizer.param_groups[0]['lr']
+        new_lr = 0.01
+        self.checkpoint_io.optimizer.param_groups[0]['lr'] = new_lr
+        assert old_lr != self.checkpoint_io.optimizer.param_groups[0]['lr']
+        self.checkpoint_io.load(self.checkpoint_io.model.epoch)
+        assert old_lr == self.checkpoint_io.optimizer.param_groups[0]['lr']
+
+    def test_checkpoint_load_optimizer_warning(self, mock_model):
+        """Test it raises a warning if the optimizer state is incompatible."""
+        self.checkpoint_io.save()
+        model_with_no_bias = torch.nn.Linear(1, 1, bias=False)
+        optimizer = torch.optim.SGD(model_with_no_bias.parameters())
+        self.checkpoint_io = checkpoint.CheckpointIO(mock_model, optimizer)
+
         with pytest.warns(exceptions.OptimizerNotLoadedWarning):
             self.checkpoint_io.load()
