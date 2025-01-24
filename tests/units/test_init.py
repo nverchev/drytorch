f00ab937c1fa8d1d8b447f773717ee3f39f28192
nverchev <nicolas.vercheval@gmail.com>
2025-01-24 15:19:37 +0100

    Added.

---
+++ tests/units/test_init.py
@@ -0,0 +1,18 @@
+"""Test functions for default trackers."""
+
+from src import dry_torch
+
+
+def test_standard_trackers():
+    """Test adding standard trackers."""
+    assert not dry_torch.DEFAULT_TRACKERS
+    dry_torch.add_standard_trackers_to_default_trackers()
+    assert dry_torch.DEFAULT_TRACKERS
+
+
+def test_remove_trackers():
+    """Test removing all default trackers."""
+    dry_torch.add_standard_trackers_to_default_trackers()
+    assert dry_torch.DEFAULT_TRACKERS
+    dry_torch.remove_all_default_trackers()
+    assert not dry_torch.DEFAULT_TRACKERS
