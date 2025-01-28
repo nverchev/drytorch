d09d291fdff2f7e622652c36feea8e0bf2370e17
nverchev <nicolas.vercheval@gmail.com>
2025-01-28 17:29:01 +0100

    Completely rewritten

--- src/dry_torch/registering.py
+++ src/dry_torch/registering.py
@@ -1,91 +1,63 @@
-"""Module with functions to connect a Model-like instance to other classes."""
-import warnings
-from collections.abc import Callable
-from functools import wraps
-from typing import Concatenate, Any, TypeVar, ParamSpec
+"""
+Module that register models and records when they are called.
 
-from src.dry_torch import log_events
+Callers and models are registered in global variables that keep track of the
+experiments at the time of calling. The experiment must be the same. Then the
+Experiment class is called to create the log events.
+
+Attributes:
+    ALL_CALLERS: A dictionary that maps caller IDs to experiments.
+    ALL_MODULES: A dictionary that maps module instances to experiments.
+"""
+
+from typing import Any
+
+from torch import nn
+
 from src.dry_torch import exceptions
 from src.dry_torch import protocols as p
+from src.dry_torch import repr_utils
 from src.dry_torch import tracking
 
-_Input_contra = TypeVar('_Input_contra',
-                        bound=p.InputType,
-                        contravariant=True)
-_Output_co = TypeVar('_Output_co',
-                     bound=p.OutputType,
-                     covariant=True)
-
-_P = ParamSpec('_P')
-_RT = TypeVar('_RT')
-
-_REGISTERED_MODELS: dict[int, tracking.Experiment] = {}
+ALL_CALLERS = dict[int, tracking.Experiment]()
+ALL_MODULES = dict[nn.Module, tracking.Experiment]()
 
 
-def register_model(model: p.ModelProtocol, /) -> None:
+def record_model_call(x: Any, model: p.ModelProtocol) -> None:
     """
-    Function needed to save train or test a Model-like instance.
-
-    It registers a model to an experiment, giving a context that encapsulates
-    the development and testing of the model.
+    Records metadata in the current experiment.
 
     Args:
-        model: the model to register.
-
-    Side Effects:
-        Creation of a ModelTracker instance.
-        Metadata about the model is dumped to a file.
+        x: The object to document.
+        model: The model that the object calls.
     """
     exp = tracking.Experiment.current()
-    model_identifier = id(model.module)
-    if model_identifier in _REGISTERED_MODELS:
-        exp_name = _REGISTERED_MODELS[model_identifier].name
-        raise exceptions.ModuleAlreadyRegisteredError(exp_name)
-    _REGISTERED_MODELS[model_identifier] = exp
-    log_events.ModelCreation(model)
+    name = getattr(x, 'name', '') or repr_utils.StrWithTS(x.__class__.__name__)
+    if id(x) not in ALL_CALLERS:
+        ALL_CALLERS[id(x)] = exp
+        exp.metadata_manager.record_model_call(name, model.name, x)
+    module = model.module
+    try:
+        model_exp = ALL_MODULES[module]
+    except KeyError:
+        raise exceptions.ModelNotRegisteredError(model.name, exp.name)
+    else:
+        if exp is not model_exp:
+            raise exceptions.ModelNotRegisteredError(model.name, exp.name)
+    return
 
 
-def register_kwargs(
-        func: Callable[
-            Concatenate[Any, p.ModelProtocol[_Input_contra, _Output_co], _P],
-            _RT],
-) -> Callable[
-    Concatenate[Any, p.ModelProtocol[_Input_contra, _Output_co], _P], _RT,
-]:
+def register_model(model: p.ModelProtocol) -> tracking.Experiment:
     """
-    Decorator that collects metadata related to a model.
-
-    This decorator should be applied to the __init__ method of a class that
-    operates on the model. The model should be the first positional argument
-    after the instance of the class. The keyword arguments are added to the
-    metadata related to the class.
+    Records mode inl the current experiment.
 
     Args:
-        func: typically the __init__ method of a class.
-    Returns:
-        the same input function.
+        model: The model to register.
     """
-
-    @wraps(func)
-    def wrapper(instance: Any,
-                model: p.ModelProtocol[_Input_contra, _Output_co],
-                *args: _P.args,
-                **kwargs: _P.kwargs) -> _RT:
-
-        if not isinstance(model, p.ModelProtocol):
-            raise exceptions.ModelFirstError(model)
-
-        if args:
-            warnings.warn(exceptions.NotDocumentedArgs())
-
-        class_name = instance.__class__.__name__
-        if 'name' in kwargs:
-            name = str(kwargs.pop('name'))
-        else:
-            name = class_name
-
-        record = log_events.RecordMetadata(model.name, class_name, name, kwargs)
-        kwargs['name'] = record.name
-        return func(instance, model, *args, **kwargs)
-
-    return wrapper
+    exp = tracking.Experiment.current()
+    module = model.module
+    if module in ALL_MODULES:
+        raise exceptions.ModelAlreadyRegisteredError(exp.name)
+    ALL_MODULES[module] = exp
+    exp.metadata_manager.register_model(model)
+    return exp
