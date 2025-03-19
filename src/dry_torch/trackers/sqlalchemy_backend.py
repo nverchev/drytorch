630616a0265546ea9f23de8cb08d074987ad289f
nverchev <nicolas.vercheval@gmail.com>
2025-03-19 13:44:11 +0100

    feat: created sqlalchemy tracker

--- src/dry_torch/trackers/sqlalchemy_backend.py
+++ src/dry_torch/trackers/sqlalchemy_backend.py
@@ -1,11 +1,14 @@
 import datetime
-import pathlib
+import functools
 from typing import Optional
+from typing_extensions import override
 
 import sqlalchemy
-from sqlalchemy import orm
+from sqlalchemy import orm, select, case, String, Float, Integer, func
+from sqlalchemy import URL
 
-from src.dry_torch.log_backend import LogBackend, ExperimentLog
+from dry_torch import log_events
+from dry_torch import tracking
 
 
 class Base(orm.DeclarativeBase):
@@ -18,96 +21,129 @@ class LoggedMetrics(orm.MappedAsDataclass, Base):
     __tablename__ = 'logged_metrics'
 
     id: orm.Mapped[int] = orm.mapped_column(
-        init=False, primary_key=True, autoincrement=True)
+        init=False,
+        primary_key=True,
+        autoincrement=True
+    )
     experiment: orm.Mapped[str] = orm.mapped_column(index=True)
     model_name: orm.Mapped[str] = orm.mapped_column(index=True)
     source: orm.Mapped[str] = orm.mapped_column(index=True)
-    partition: orm.Mapped[str] = orm.mapped_column(index=True)
     epoch: orm.Mapped[int] = orm.mapped_column()
-    metric_name: orm.Mapped[str] = orm.mapped_column()
+    metric_name: orm.Mapped[str] = orm.mapped_column(index=True)
     metric_value: orm.Mapped[float] = orm.mapped_column()
     timestamp: orm.Mapped[datetime.datetime] = orm.mapped_column(
-        init=False, insert_default=sqlalchemy.func.now())
+        init=False,
+        insert_default=sqlalchemy.func.now()
+    )
 
 
-class SQLiteBackend(LogBackend):
-    """Logging backend implementation using SQLite with SQLAlchemy."""
+class SQLConnection(tracking.Tracker):
+    """Tracker that dumps metrics into a SQL database."""
 
-    def __init__(self, engine: sqlalchemy.Engine, exp_name: str) -> None:
-        self.engine = engine
-        self.exp_name = exp_name
-        self.Table = LoggedMetrics
-        self.Session = orm.sessionmaker(bind=engine)
-        Base.metadata.create_all(bind=engine)
+    def __init__(self,
+                 drivername: str = 'sqlite',
+                 username: Optional[str] = None,
+                 password: Optional[str] = None,
+                 host: Optional[str] = None,
+                 database: Optional[str] = None) -> None:
+        """
+        Args:
+            drivername: see sqlalchemy.engine.URL.create documentation
+            username: see sqlalchemy.engine.URL.create documentation
+            password: see sqlalchemy.engine.URL.create documentation
+            host: see sqlalchemy.engine.URL.create documentation
+            database: see sqlalchemy.engine.URL.create documentation
+        """
+        super().__init__()
+        self.url = URL.create(drivername,
+                              username=username,
+                              password=password,
+                              host=host,
+                              database=database)
+        self.engine = sqlalchemy.create_engine(self.url)
+        self.Session = orm.sessionmaker(bind=self.engine)
+        Base.metadata.create_all(bind=self.engine)
+        self._exp_name: str | None = None
 
-    def __call__(self,
-                 model_name: str,
-                 source: str,
-                 partition: str,
-                 epoch: int,
-                 metrics: dict[str, float]) -> None:
-        session = self.Session()
-        for i, (metric_name, value) in enumerate(metrics.items()):
-            new_row = self.Table(experiment=self.exp_name,
-                                 model_name=model_name,
-                                 source=source,
-                                 partition=partition,
-                                 epoch=epoch,
-                                 metric_name=metric_name,
-                                 metric_value=value)
-            session.add(new_row)
+    @override
+    @functools.singledispatchmethod
+    def notify(self, event: log_events.Event) -> None:
+        return super().notify(event)
 
-        session.commit()
-        session.close()
+    @notify.register
+    def _(self, event: log_events.StartExperiment) -> None:
+        self._exp_name = event.exp_name
+        return super().notify(event)
 
-    def print_views(self) -> None:
-        distinct_combinations = self.Session().query(
-            LoggedMetrics.model_name,
-            LoggedMetrics.source,
-            LoggedMetrics.partition
-        ).where(LoggedMetrics.experiment == self.exp_name).distinct().all()
-        print(distinct_combinations)
+    @notify.register
+    def _(self, event: log_events.StopExperiment) -> None:
+        self._exp_name = None
+        return super().notify(event)
+
+    @notify.register
+    def _(self, event: log_events.EpochMetrics) -> None:
+        if self._exp_name is None:
+            raise RuntimeError('Access outside experiment scope.')
+        with self.Session() as session:
+            for i, (metric_name, value) in enumerate(event.metrics.items()):
+                new_row = LoggedMetrics(experiment=self._exp_name,
+                                        model_name=event.model_name,
+                                        source=event.source,
+                                        epoch=event.epoch,
+                                        metric_name=metric_name,
+                                        metric_value=value)
+                session.add(new_row)
+
+            session.commit()
         return
 
-    def plot(self,
-             model_name: str,
-             partition: str,
-             metric: str) -> None:
-        pass
+    def create_view(self):
+        ...
 
+    def get_metrics(
+            self,
+            model_name: str,
+            source: str,
+            exp_name: Optional[str] = None,
+    ) -> tuple[list[int], dict[str, list[float]]]:
+        """
+        Get a pivoted view of metrics.
 
-class SQLiteLog(ExperimentLog):
+        Args:
+            exp_name: name of the experiment. Defaults to current one.
+            model_name: name of the model
+            source: name of the source
 
-    def __init__(self, storage='sqlite:///'):
-        self.storage = storage
+        Returns:
+            epochs and metrics
+        """
+        if exp_name is not None:
+            if self._exp_name is None:
+                raise RuntimeError('Access outside experiment scope.')
+            exp_name = self._exp_name
 
-    def create_log(self,
-                   local_path: Optional[pathlib.Path],
-                   exp_name: str) -> LogBackend:
-        if local_path is None:
-            db_path = ':memory:'
-        else:
-            db_path = (local_path / 'log.db').absolute().as_posix()
-
-        engine = sqlalchemy.create_engine(self.storage + db_path)
-        return SQLiteBackend(engine, exp_name)
-
-
-def my_test():
-    exp_log = SQLiteLog()
-    backend1 = exp_log.create_log(pathlib.Path(), exp_name='first')
-    backend2 = exp_log.create_log(pathlib.Path().absolute().parent,
-                                  exp_name='second')
-    backend1('model_name',
-             'train',
-             'split',
-             1,
-             {'loss': 0.1, 'accuracy': 0.9})
-    backend2('another_model',
-             'train',
-             'split',
-             1,
-             {'loss': 0.1, 'accuracy': 0.9})
-
-
-my_test()
+        with self.Session() as session:
+            # Get all distinct metric names for this experiment
+            query = select(LoggedMetrics.epoch,
+                           LoggedMetrics.metric_name,
+                           LoggedMetrics.metric_value)
+            if exp_name is not None:
+                query = query.where(
+                    LoggedMetrics.model_name == model_name,
+                )
+            if model_name is not None:
+                query = query.where(
+                    LoggedMetrics.model_name == model_name,
+                )
+            if source is not None:
+                query = query.where(
+                    LoggedMetrics.source == source,
+                )
+            epochs = list[int]()
+            metrics = dict[str, list[float]]()
+            for row in session.execute(query):
+                if not epochs or epochs[-1] != row[0]:
+                    epochs.append(row[0])
+                metrics.setdefault(row[1], []).append(row[2])
+            assert {len(values) for values in metrics.values()} == {len(epochs)}
+            return epochs, metrics
