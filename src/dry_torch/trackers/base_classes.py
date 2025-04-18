147a8582a6909261b4294ab94f7f93d5540e0952
nverchev <nicolas.vercheval@gmail.com>
2025-04-18 16:24:54 +0200

    Created

---
+++ src/dry_torch/trackers/base_classes.py
@@ -0,0 +1,243 @@
+"""Abstract classes for trackers."""
+
+import abc
+import functools
+import pathlib
+from typing import Optional, Iterable
+
+from typing_extensions import override
+
+from dry_torch import exceptions
+from dry_torch import experiments
+from dry_torch import log_events
+from dry_torch import tracking
+
+LogTuple = tuple[list[int], dict[str, list[float]]]
+
+
+class AbstractDumper(tracking.Tracker, metaclass=abc.ABCMeta):
+    """Tracker that for a custom directory."""
+
+    def __init__(self, par_dir: Optional[pathlib.Path] = None):
+        """
+        Args:
+            par_dir: Directory where to dump metadata. Defaults uses the one of
+                the current experiment.
+        """
+        super().__init__()
+        self._par_dir = par_dir
+        self._exp_dir: Optional[pathlib.Path] = None
+
+    @property
+    def par_dir(self) -> pathlib.Path:
+        """Return the directory where the files will be saved."""
+        if self._par_dir is None:
+            if self._exp_dir is None:
+                raise exceptions.AccessOutsideScopeError()
+            path = self._exp_dir
+        else:
+            path = self._par_dir
+        path.mkdir(exist_ok=True)
+        return path
+
+    @override
+    @functools.singledispatchmethod
+    def notify(self, event: log_events.Event) -> None:
+        return super().notify(event)
+
+    @notify.register
+    def _(self, event: log_events.StartExperiment) -> None:
+        self._exp_dir = event.exp_dir
+
+    @notify.register
+    def _(self, _: log_events.StopExperiment) -> None:
+        self._exp_dir = None
+
+
+class MetricLoader(tracking.Tracker, abc.ABC):
+    """Interface for trackers that load metrics."""
+
+    def load_metrics(self,
+                     model_name: str,
+                     max_epoch: int = -1) -> dict[str, LogTuple]:
+        """
+        Loads metrics stored by the tracker.
+
+        Args:
+            model_name: name of the model.
+            max_epoch: maximum epoch to load. Defaults to all.
+
+        Returns:
+            current epochs and named metric values by source.
+
+        Raises:
+            AccessOutsideScopeError if called outside the experiment scope.
+        """
+        try:
+            experiments.Experiment.current()
+        except exceptions.NoActiveExperimentError:
+            raise exceptions.AccessOutsideScopeError()
+
+        if max_epoch == 0:
+            return {}
+        if max_epoch < -1:
+            msg = 'Max epoch should not be less than -1.'
+            raise exceptions.TrackerException(self, msg)
+        return self._load_metrics(model_name, max_epoch)
+
+    @abc.abstractmethod
+    def _load_metrics(self,
+                      model_name: str,
+                      max_epoch: int = -1) -> dict[str, LogTuple]:
+        ...
+
+
+class MemoryMetrics(tracking.Tracker):
+    """Keeps all metrics in memory."""
+
+    def __init__(self, metric_loader: Optional[MetricLoader] = None) -> None:
+        super().__init__()
+        self.metric_loader = metric_loader
+        self.model_metrics = dict[str, dict[str, LogTuple]]()
+
+    @override
+    @functools.singledispatchmethod
+    def notify(self, event: log_events.Event) -> None:
+        return super().notify(event)
+
+    @notify.register
+    def _(self, event: log_events.Metrics) -> None:
+        model_name = format(event.model_name, 's')
+        event_source = format(event.source, 's')
+        source_dict = self.model_metrics.setdefault(model_name, {})
+        epochs, logs_dict = source_dict.setdefault(event_source, ([], {}))
+        epochs.append(event.epoch)
+        for metric_name, metric_value in event.metrics.items():
+            logs_dict.setdefault(metric_name, []).append(metric_value)
+        return
+
+    @notify.register
+    def _(self, event: log_events.LoadModel) -> None:
+        if self.metric_loader is None:
+            return
+        metrics = self.metric_loader.load_metrics(event.model_name, event.epoch)
+        self.model_metrics[event.model_name] = metrics
+        return
+
+
+class BasePlotter(MemoryMetrics):
+    """Abstract class for plotting trajectory from sources. """
+
+    def __init__(self,
+                 model_names: Iterable[str] = (),
+                 metric_names: Iterable[str] = (),
+                 metric_loader: Optional[MetricLoader] = None,
+                 start_epoch: int = 1) -> None:
+        """
+        Args:
+            model_names: the names of the models to plot. Defaults to all.
+            metric_names: the names of the metrics to plot. Defaults to all.
+            metric_loader: a tracker that can load metrics from a previous run.
+            start_epoch: epoch from which plot starts.
+
+        Note:
+            start_epoch allows you to exclude the initial epochs from the graph.
+            During the first 2 * start_epoch epochs, the graph is shown in
+            its entirety.
+        """
+        super().__init__(metric_loader)
+        self.model_names = model_names
+        self.metric_names = metric_names
+        self.start = start_epoch
+
+    @override
+    @functools.singledispatchmethod
+    def notify(self, event: log_events.Event) -> None:
+        return super().notify(event)
+
+    @notify.register
+    def _(self, event: log_events.EndEpoch) -> None:
+        super().notify(event)
+        model_name = format(event.model_name, 's')
+        if self.model_names and model_name not in self.model_names:
+            return
+        source_dict = self.model_metrics.get(model_name, {})
+        start = 1 if self.start < 2 * event.epoch else self.start
+        if source_dict:
+            if self.metric_names:
+                metric_names: Iterable[str] = self.metric_names
+            else:
+                all_metrics = (list(logs[1]) for logs in source_dict.values())
+                metric_names = sum(all_metrics, [])
+
+            for metric_name in metric_names:
+                filtered_sources = self._filter_epochs_and_metrics(
+                    source_dict,
+                    metric_name=metric_name,
+                    start=start
+                )
+                self._plot_metric(model_name, metric_name, **filtered_sources)
+        return
+
+    def plot(self,
+             model_name: str,
+             source_names: Iterable[str] = (),
+             metric_names: Iterable[str] = (),
+             start: int = 1) -> None:
+        """
+        Plots the learning curves.
+
+        Args:
+            model_name: the name of the model to plot.
+            source_names: the names of the sources to plot. Defaults to all.
+            metric_names: the metric to plot. Defaults to all.
+            start: the epoch from which to start plotting. Defaults to 1.
+        """
+        model_sources = self.model_metrics.get(model_name, {})
+        if not model_sources and self.metric_loader is not None:
+            model_sources = self.metric_loader.load_metrics(model_name)
+        if not model_sources:
+            msg = f'No model named {model_name} has been found.'
+            raise exceptions.TrackerException(self, msg)
+        if source_names:
+            model_sources = {source: logs
+                             for source, logs in model_sources.items()
+                             if source in source_names}
+        if metric_names:
+            metric_names: Iterable[str] = metric_names
+        else:
+            all_metrics = (list(logs[1]) for logs in model_sources.values())
+            metric_names = sum(all_metrics, [])
+
+        for metric_name in metric_names:
+            filtered_sources = self._filter_epochs_and_metrics(model_sources,
+                                                               metric_name,
+                                                               start)
+
+            self._plot_metric(model_name, metric_name, **filtered_sources)
+
+    @abc.abstractmethod
+    def _plot_metric(self,
+                     model_name: str,
+                     metric_name: str,
+                     **sources: tuple[list[int], list[float]]) -> None:
+        ...
+
+    @staticmethod
+    def _filter_epochs_and_metrics(
+            sourced_metrics: dict[str, LogTuple],
+            metric_name: str = 'Loss',
+            start: int = 0,
+    ) -> dict[str, tuple[list[int], list[float]]]:
+
+        source_dict = dict[str, tuple[list[int], list[float]]]()
+        metric_dict: dict[str, list[float]]
+        for source_name, (epochs, metric_dict) in sourced_metrics.items():
+            new_epochs = list[int]()
+            values = list[float]()
+            for epoch, value in zip(epochs, metric_dict[metric_name]):
+                if epoch >= start:
+                    new_epochs.append(epoch)
+                    values.append(value)
+            source_dict[source_name] = (new_epochs, values)
+        return source_dict
