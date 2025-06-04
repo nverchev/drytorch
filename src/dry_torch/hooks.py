0a49170f475e1ad4e98582e7edc21a7cf4c22167
nverchev <nicolas.vercheval@gmail.com>
2025-06-04 15:41:55 +0200

    feat: added get_moving_average

--- src/dry_torch/hooks.py
+++ src/dry_torch/hooks.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 import abc
 from collections.abc import Callable, Sequence
+import math
 import operator
 from typing import Generic, Literal, Optional, ParamSpec, TypeVar, cast
 from typing_extensions import override
@@ -707,7 +708,7 @@ class RestartScheduleOnPlateau(ChangeSchedulerOnPlateauCallback):
     """
     Restart the scheduling after plateauing.
 
-    Attribute:
+    Attributes:
         monitor: monitor instance.
         cooldown: number of calls to skip after changing the schedule.
     """
@@ -727,16 +728,65 @@ class RestartScheduleOnPlateau(ChangeSchedulerOnPlateauCallback):
         return schedulers.WarmupScheduler(epoch, scheduler)
 
 
-def mean_aggregation(window: int) -> Callable[[Sequence[float]], float]:
+def get_sliding_mean(window_size: int) -> Callable[[Sequence[float]], float]:
     """
-    Aggregate recent values by taking the mean.
+    Return a sliding window average by specifying window size.
 
     Args:
-        window: number of items to aggregate.
+        window_size: number of items to aggregate.
+
+    Returns:
+        The windowed average function.
+
+    Raises:
+        ValueError if window size is negative.
     """
+    if window_size < 0:
+        raise ValueError('Window size must be positive.')
 
     def _mean(float_list: Sequence[float], /) -> float:
-        clipped_window = min(window, len(float_list))
+        clipped_window = min(window_size, len(float_list))
         return sum(float_list[-clipped_window:]) / clipped_window
+
+    return _mean
+
+
+def get_moving_average(
+        decay: float = 0.9,
+        threshold: float = 1e-4,
+) -> Callable[[Sequence[float]], float]:
+    """
+    Return a moving average by specifying the decay.
+
+    Args:
+        decay: the closer to 0 the more the last elements have weight.
+        threshold: value below which the weight is considered negligible.
+
+    Returns:
+        The moving average function.
+
+    Raises:
+        ValueError if decay is not between 0 and 1.
+        ValueError if threshold is negative or immediately hit.
+    """
+    if not 0 < decay < 1:
+        raise ValueError('Decay must be between 0 and 1.')
+
+    if not 0 <= threshold <= decay * (1 - decay):
+        raise ValueError('Threshold should be small and non-negative.')
+
+    # how far back to go back before the weight drops below the threshold
+    stop = int(math.log(threshold / (1 - decay), decay)) + 2 if threshold else 0
+
+    def _mean(float_list: Sequence[float], /) -> float:
+        total: float = 0
+        total_weights: float = 0  # should get close to one
+        weight = 1 - decay  # weights are normalized
+        for elem in float_list[:-stop:-1]:
+            total += weight * elem
+            total_weights += weight
+            weight *= decay
+
+        return total / total_weights
 
     return _mean
