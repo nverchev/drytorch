8051559c2e8ba1dbb4fbf04f98fc6b6a2b9c4aa6
nverchev <nicolas.vercheval@gmail.com>
2025-06-05 21:01:25 +0200

    refactor: public to private attrs

--- src/dry_torch/utils/repr_utils.py
+++ src/dry_torch/utils/repr_utils.py
@@ -74,25 +74,25 @@ else:
                      precision: int = 3,
                      max_rows: int = 10,
                      max_columns: int = 10) -> None:
-            self.options: dict[str, int] = {
+            self._options: dict[str, int] = {
                 'display.precision': precision,
                 'display.max_rows': max_rows,
                 'display.max_columns': max_columns,
             }
-            self.original_options: dict[str, Any] = {}
+            self._original_options: dict[str, Any] = {}
 
         def __enter__(self) -> None:
-            self.original_options.update(
-                {key: pd.get_option(key) for key in self.options}
+            self._original_options.update(
+                {key: pd.get_option(key) for key in self._options}
             )
-            for key, value in self.options.items():
+            for key, value in self._options.items():
                 pd.set_option(key, value)
 
         def __exit__(self,
                      exc_type: None = None,
                      exc_val: None = None,
                      exc_tb: None = None) -> None:
-            for key, value in self.original_options.items():
+            for key, value in self._original_options.items():
                 pd.set_option(key, value)
 
     PandasObject = pd.core.base.PandasObject
