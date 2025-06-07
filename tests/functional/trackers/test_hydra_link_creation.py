1f163b5e1b85ec350fcc3cc41047b6742131b932
nverchev <nicolas.vercheval@gmail.com>
2025-06-07 16:58:28 +0200

    created

---
+++ tests/functional/trackers/test_hydra_link_creation.py
@@ -0,0 +1,46 @@
+"""Tests for the HydraLink creates a new local folder."""
+
+import pytest
+
+import sys
+
+import hydra
+from omegaconf import DictConfig
+
+from dry_torch import Experiment
+from dry_torch.trackers.hydra import HydraLink
+
+
+class TestHydraLinkFolderCreation:
+    """Tests HydraLink creates a folder."""
+
+    @pytest.fixture(autouse=True)
+    def full_cycle(self,
+                   tmp_path_factory,
+                   monkeypatch) -> None:
+        """Setup test environment with actual hydra configuration."""
+        self.hydra_dir = tmp_path_factory.mktemp('outputs')
+        run_dir_arg = f'++hydra.run.dir={self.hydra_dir.as_posix()}'
+        self.exp = Experiment[None](par_dir=tmp_path_factory.mktemp('exp'))
+        self.exp.trackers.named_trackers.clear()
+        with monkeypatch.context() as m:
+            m.setattr(sys, 'argv', ['test_script', run_dir_arg])
+
+            @hydra.main(version_base=None)
+            def _app(_: DictConfig):
+                self.exp.trackers.register(HydraLink())
+                with self.exp:
+                    pass
+
+                return
+
+            _app()
+        return
+
+    def test_log_file(self) -> None:
+        """Test HydraLink creates file log with expected format."""
+        exp_dir = self.exp.dir
+        hydra_folder = HydraLink.hydra_folder
+        link_name = HydraLink.link_name
+        linked_folder = exp_dir / hydra_folder / link_name
+        assert linked_folder.exists()
