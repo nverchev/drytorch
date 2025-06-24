2c1752c18730da2ab0a98abc9c24f3baea43e7a1
nverchev <nicolas.vercheval@gmail.com>
2025-06-24 19:48:10 +0200

    feat: added contrib folder with swa utils and tests.

---
+++ src/drytorch/contrib/__init__.py
@@ -0,0 +1,1 @@
+"""Contains extension and contributions"""
---
+++ src/drytorch/contrib/swa_utils.py
@@ -0,0 +1,31 @@
+"""Utilities for Stochastic Weight Averaging"""
+
+import torch
+
+from drytorch.running import ModelCaller
+
+
+class ModelMomentaUpdater(ModelCaller):
+
+    def __call__(self) -> None:
+        """Single pass on the dataset."""
+        momenta = dict[torch.nn.Module, float]()
+        for module in self.model.module.modules():
+            if isinstance(module, torch.nn.modules.batchnorm._BatchNorm):
+                module.reset_running_stats()
+                momenta[module] = module.momentum
+
+        if not momenta:
+            return
+
+        was_training = self.model.module.training
+        self.model.module.train()
+        for module in momenta.keys():
+            module.momentum = None
+
+        super().__call__()
+        for bn_module in momenta.keys():
+            bn_module.momentum = momenta[bn_module]
+
+        self.model.module.train(was_training)
+        return
