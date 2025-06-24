dda1c5b0521b7699c31adddc2940fba8aae89946
nverchev <nicolas.vercheval@gmail.com>
2025-06-24 19:54:45 +0200

    refactor: created from_torchmetrics.py and tests.

--- src/drytorch/metrics.py
+++ src/drytorch/metrics.py
@@ -13,8 +13,7 @@ import abc
 from collections.abc import Callable, Hashable
 import copy
 import operator
-from typing import Any, Mapping, Protocol, Optional, Self, TypeVar
-from typing import runtime_checkable
+from typing import Mapping, Optional, Self, TypeVar
 from typing_extensions import override
 import warnings
 
@@ -37,85 +36,6 @@ _Target = TypeVar('_Target', bound=p.TargetType)
 _Output = TypeVar('_Output', bound=p.OutputType)
 
 _Tensor = torch.Tensor
-
-
-@runtime_checkable
-class TorchMetricCompositionalMetricProtocol(Protocol):
-    """
-    Protocol for a compositional metric from torchmetrics.
-
-    Attributes:
-        metric_a: first metric.
-        metric_b: second metric.
-    """
-    metric_a: p.OjectiveProtocol | float | None
-    metric_b: p.OjectiveProtocol | float | None
-
-    def update(self,
-               outputs: _Tensor,
-               targets: _Tensor) -> Any:
-        """See torchmetrics documentation."""
-
-    def reset(self) -> Any:
-        """See torchmetrics documentation."""
-
-    def compute(self) -> Mapping[str, _Tensor] | _Tensor | None:
-        """See torchmetrics documentation."""
-
-    def forward(self,
-                outputs: _Tensor,
-                targets: _Tensor) -> _Tensor:
-        """See torchmetrics documentation."""
-
-    def __call__(self,
-                 outputs: _Tensor,
-                 targets: _Tensor) -> _Tensor:
-        """See torchmetrics documentation."""
-
-
-def from_torchmetrics(
-        metric: TorchMetricCompositionalMetricProtocol
-) -> p.LossCalculatorProtocol[_Tensor, _Tensor]:
-    """Returns a wrapper of a CompositionalMetric for integration."""
-
-    class _TorchMetricCompositionalMetric(
-        p.LossCalculatorProtocol[_Tensor, _Tensor]
-    ):
-        name = 'Loss'
-
-        def __init__(self, _metric: TorchMetricCompositionalMetricProtocol):
-            self.metric = _metric
-
-        def update(self,
-                   outputs: _Tensor,
-                   targets: _Tensor) -> Any:
-            self.metric.update(outputs, targets)
-
-        def reset(self) -> Any:
-            self.metric.reset()
-
-        def forward(self,
-                    outputs: _Tensor,
-                    targets: _Tensor) -> _Tensor:
-            return self.metric(outputs, targets)
-
-        def compute(self) -> dict[str, _Tensor]:
-            dict_output = dict[str, _Tensor]()
-            metric_list = list[p.OjectiveProtocol | float | None]()
-            metric_list.append(self.metric)
-            while metric_list:
-                metric_ = metric_list.pop()
-                if isinstance(metric_, self.metric.__class__):
-                    metric_list.extend([metric_.metric_b, metric_.metric_a])
-                elif isinstance(metric_, (float, int)) or metric_ is None:
-                    continue
-                else:
-                    if isinstance(value := metric_.compute(), _Tensor):
-                        dict_output[metric_.__class__.__name__] = value
-
-            return dict_output
-
-    return _TorchMetricCompositionalMetric(metric)
 
 
 class Objective(
--- tests/units/test_metrics.py
+++ tests/units/test_metrics.py
@@ -9,7 +9,7 @@ from drytorch import exceptions
 from drytorch import protocols as p
 from drytorch.metrics import Metric, MetricCollection
 from drytorch.metrics import Loss, CompositionalLoss
-from drytorch.metrics import dict_apply, from_torchmetrics
+from drytorch.metrics import dict_apply
 from drytorch.metrics import repr_metrics
 
 
@@ -39,49 +39,6 @@ def metric_fun_2(metric_2: str) -> dict[str, Callable[
 ]:
     """Another simple metric fun."""
     return {metric_2: lambda x, y: y}
-
-
-class TestFromTorchMetrics:
-    @pytest.fixture(autouse=True)
-    def setup(self) -> None:
-        """Set up a torchmetrics-based test fixture."""
-
-        # Conditionally import torchmetrics
-        torchmetrics = pytest.importorskip('torchmetrics')
-
-        self.metric_a = torchmetrics.Accuracy(task='binary')
-        self.metric_b = torchmetrics.MeanSquaredError()
-        self.additive_metric = 2 * self.metric_a + self.metric_b
-        self.metric = from_torchmetrics(self.additive_metric)
-        return
-
-    def test_update_and_compute(self) -> None:
-        """Test it correctly updates and computes metrics as dictionaries."""
-        mock_outputs = torch.tensor([0.1, 0.2])
-        mock_targets = torch.tensor([1, 0])
-
-        self.metric.update(mock_outputs, mock_targets)
-        result = self.metric.compute()
-
-        # Note that update modifies the state of the input metric
-        expected = {self.metric_a.__class__.__name__: self.metric_a.compute(),
-                    self.metric_b.__class__.__name__: self.metric_b.compute()}
-
-        assert result == expected
-
-    def test_forward(self) -> None:
-        """Test that forward still outputs a Tensor with the correct value."""
-        mock_outputs = torch.tensor([0.1, 0.2])
-        mock_targets = torch.tensor([1, 0])
-
-        # Update the wrapped metric
-        result = self.metric.forward(mock_outputs, mock_targets)
-
-        # Note that forward modifies the state of the input metric
-        expected = self.additive_metric.compute()
-
-        assert isinstance(result, torch.Tensor)
-        assert torch.allclose(result, expected)
 
 
 class TestMetricCollection:
