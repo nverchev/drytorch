f3ff39912a0558356ed1fe43eb6fc69b977f1829
nverchev <nicolas.vercheval@gmail.com>
2025-07-03 16:42:54 +0200

    created

---
+++ tests/integration/test_local_checkpoint.py
@@ -0,0 +1,77 @@
+"""Tests LocalCheckpoint integration with the Model and ModelOptimizer class."""
+from typing import Generator
+
+import pytest
+
+import drytorch
+from drytorch import Experiment
+from drytorch.models import ModelOptimizer
+
+
+@pytest.fixture(autouse=True, scope='module')
+def experiment(tmpdir_factory) -> Generator[Experiment, None, None]:
+    """Fixture of an experiment."""
+    drytorch.remove_all_default_trackers()
+    par_dir = tmpdir_factory.mktemp('experiments')
+    with Experiment[None](name='TestExperiment', par_dir=par_dir) as exp:
+        yield exp
+    return
+
+
+def test_state_save_and_load(linear_model):
+    param_list = [param.clone() for param in linear_model.module.parameters()]
+
+    linear_model.save_state()
+
+    # change param values
+    for param in linear_model.module.parameters():
+        param.data.fill_(1)
+
+    # increase epoch
+    linear_model.increment_epoch()
+    incremented_epoch = linear_model.epoch
+
+    # check params have changed
+    for param, old_param in zip(linear_model.module.parameters(), param_list):
+        assert param != old_param
+
+    linear_model.load_state()
+
+    # check original params and epoch
+    assert linear_model.epoch < incremented_epoch
+    for param, old_param in zip(linear_model.module.parameters(), param_list):
+        assert param == old_param
+
+
+def test_checkpoint_save_and_load(linear_model, standard_learning_scheme):
+    model_optimizer = ModelOptimizer(linear_model, standard_learning_scheme)
+
+    param_list = [param.clone() for param in linear_model.module.parameters()]
+    optimizer = model_optimizer._optimizer
+    optim_groups = optimizer.param_groups[0].copy()
+
+    model_optimizer.save()
+
+    # change param values
+    for param in linear_model.module.parameters():
+        param.data.fill_(1)
+
+    # increase epoch
+    linear_model.increment_epoch()
+    incremented_epoch = linear_model.epoch
+
+    # modify optimizer state
+    optimizer.param_groups[0]['lr'] = 1
+
+    # check params have changed
+    for param, old_param in zip(linear_model.module.parameters(), param_list):
+        assert param != old_param
+    assert optimizer.param_groups[0]['lr'] != optim_groups['lr']
+
+    model_optimizer.load()
+
+    # check original params and epoch
+    assert linear_model.epoch < incremented_epoch
+    for param, old_param in zip(linear_model.module.parameters(), param_list):
+        assert param == old_param
+    assert optimizer.param_groups[0]['lr'] == optim_groups['lr']
