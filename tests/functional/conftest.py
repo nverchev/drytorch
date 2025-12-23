f545af412c6ad08aae80de1e8d31f751dc1fea13
nverchev <nicolas.vercheval@gmail.com>
2025-12-23 18:34:25 +0100

    test: specify windows preferred gloo settings

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -3,6 +3,7 @@
 import os
 import pathlib
 import socket
+import sys
 import uuid
 import warnings
 
@@ -174,6 +175,9 @@ class DistributedWorker(Generic[P, T]):
     def _setup_distributed(self, rank: int) -> None:
         os.environ['MASTER_ADDR'] = 'localhost'
         os.environ['MASTER_PORT'] = self.port
+        if sys.platform == 'win32':
+            os.environ.setdefault('GLOO_SOCKET_IFNAME', 'lo')
+
         dist.init_process_group(
             backend='gloo',
             rank=rank,
